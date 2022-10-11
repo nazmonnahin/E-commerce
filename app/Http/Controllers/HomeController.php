a480a28045e5d9f7efa0ef1625cc3afc36973eec
nazmonnahin <nntushar305@gmail.com>
2022-10-12 00:27:49 +0600

    11 oct

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        $usertype = Auth::user()->usertype;
+
+        if ($usertype=='1') {
+
+            return view('admin.home');
+
+        }
+
+        else{
+
+            return view('dashboard');
+        }
+    }
+}
